e40dd2d129d3b432197062272f4a9abebed3cac5
U-LAPTOP-GAU0OU56\gamer <gamerwoowoo6@gmail.com>
2021-04-15 17:49:11 -0500

    nothing here...

--- RGproto.js
+++ RGproto.js
@@ -7,7 +7,7 @@ function windowResized() {
 }
 
 function draw() {
-	background(0);
+	background(0.15);
   if (mouseIsPressed) {
     fill(255);
 		stroke(10);
@@ -16,4 +16,12 @@ function draw() {
 		noStroke();
   }
   ellipse(mouseX, mouseY, 80, 80);
+  
+  let title = "Nothing here...";
+  
+  textSize(32);
+  for(let i = 1; i > 0; i-=0.1){
+	fill(0, i*200, i*255);
+    text(title, 10, 30+i*400);
+  }
 }
